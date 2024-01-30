1d8343e5d16bfc0838180dc277e08e6b13886154
Roy Brabson <brabson@alumni.duke.edu>
2024-01-30 17:41:15 -0500

    group test opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/FirstVisionOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/FirstVisionOpMode.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
 
-@Autonomous (name="First Vision Processor", group="vision")
+@Autonomous (name="First Vision Processor", group="test")
 public class FirstVisionOpMode extends OpMode {
     private FirstVisionProcessor visionProcessor;
     private VisionPortal visionPortal;
